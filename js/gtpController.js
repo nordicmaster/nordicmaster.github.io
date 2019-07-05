8afb1d6c2000d05cc867d138d1a663600b372b9b
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2019-07-05 17:22:33 +0300

    Update gtpController.js

--- js/gtpController.js
+++ js/gtpController.js
@@ -16,7 +16,7 @@ myApp.controller('gtpController', function($scope) {
         $scope.gtp.text = 'newtab';
     }
     $scope.fetchJson = function (e) {
-        $scope.textJson.text = '<a href="https://nordicmaster.github.io/table_items.json">json</a>';
-        //JSON.parse("https://nordicmaster.github.io/table_items.json");
+        $scope.textJson.text = JSON.parse("https://nordicmaster.github.io/table_items.json");
+        //'<a href="https://nordicmaster.github.io/table_items.json">json</a>';
     }
 });
