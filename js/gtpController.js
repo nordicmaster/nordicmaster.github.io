96bf4e5036c76eeffee0a778ebd5cc2401bd42f1
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2019-07-10 14:14:15 +0300

    Update gtpController.js

--- js/gtpController.js
+++ js/gtpController.js
@@ -12,14 +12,6 @@ myApp.controller('gtpController', function($scope) {
         author: 'NM',
         date: '20/10/2019'
     };
-    $scope.$on('$viewContentLoaded', function() {
-        console.log("view content loaded");
-        $scope.fetchJson();
-    });
-    $scope.$on('$stateChangeSuccess', function () {
-        console.log("state change success");
-        $scope.fetchJson();
-    });
     $scope.changeClass = function (e) {         
         $scope.gtp.text = 'newtab';
     }
@@ -32,27 +24,15 @@ myApp.controller('gtpController', function($scope) {
         //'<a href="https://nordicmaster.github.io/table_items.json">json</a>';
         console.log("fetchJson");
         var xmlhttp = new XMLHttpRequest();
-        var str = "";
         var myObj;
         xmlhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
             //console.log(this.responseText);
             myObj = JSON.parse(this.responseText);            
-            str = myObj[0].name;
-            console.log("str now = "+ str);
             assignJson(myObj);            
           }
         };
         xmlhttp.open("GET", "https://nordicmaster.github.io/table_items.json", true);
         xmlhttp.send();
-        console.log("str = "+str);
-        //console.log(myObj[0].name);
-        //$scope.textJson = myObj;
     }
-    var init = function () {
-        console.log("init");
-        $scope.fetchJson();
-    };
-    // and fire it after definition
-    init();
 });
