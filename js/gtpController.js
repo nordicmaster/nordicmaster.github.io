ad9aecdede2564dd3d805fa83d7916264d218911
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2019-09-02 14:05:46 +0300

    Update gtpController.js

--- js/gtpController.js
+++ js/gtpController.js
@@ -53,8 +53,8 @@ myApp.controller('gtpController', function($scope) {
                 }
                 newstr = newstr.concat(c); 
                 i=i+1;
-            }
-            item.text=newstr;*/
+            }*/
+            item.text=str.replace(/\\n/g, '\n'));
             console.log("2: "+item.text);
           })
           .catch(function() {
