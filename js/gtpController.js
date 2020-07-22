48251fe14fd21280d3f1abf5ff774bd9a682f4f7
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2020-07-22 16:27:46 +0300

    Update gtpController.js

--- js/gtpController.js
+++ js/gtpController.js
@@ -18,7 +18,6 @@ myApp.controller('gtpController', function($scope) {
         $scope.gtp.text = 'newtab';
     }
     function assignJson(obj) {
-        //console.log("assign " + obj[1]);
         $scope.textJson = obj;
         //$scope.textJson.forEach((item,index)=>{console.log("before "+item.ID+" "+item.text)});
         $scope.textJson.forEach(addtext);
@@ -39,16 +38,6 @@ myApp.controller('gtpController', function($scope) {
             $scope.totalrecorded++;
     }
     function addtext(item, index) {
-        //console.log("add Text");
-        /*var xmlhttp2 = new XMLHttpRequest();
-        xmlhttp2.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                item.text =this.responseText; 
-                console.log(item.text);
-            }
-        }
-        xmlhttp2.open("GET", "https://nordicmaster.github.io/src/"+ item.ID+ ".txt", true);
-        xmlhttp2.send();*/
         ajax("https://nordicmaster.github.io/src/"+ item.ID+ ".txt")
           .then(function(result) {
             //console.log(JSON.stringify(result));
@@ -77,7 +66,6 @@ myApp.controller('gtpController', function($scope) {
     }
     
     function ajax(url) {
-        //console.log("aja X");
       return new Promise(function(resolve, reject) {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
@@ -92,14 +80,10 @@ myApp.controller('gtpController', function($scope) {
     }
     
     $scope.fetchJson = function () {
-        //$scope.textJson.text = JSON.parse("https://nordicmaster.github.io/table_items.json");
-        //'<a href="https://nordicmaster.github.io/table_items.json">json</a>';
-        //console.log("fetchJson");
         var xmlhttp = new XMLHttpRequest();
         var myObj;
         xmlhttp.onreadystatechange = function() {
           if (this.readyState == 4 && this.status == 200) {
-            //console.log(this.responseText);
             myObj = JSON.parse(this.responseText);            
             assignJson(myObj);            
           }
@@ -122,13 +106,11 @@ myApp.controller('gtpController', function($scope) {
         xmlhttp.send();
     }
     $scope.changeParam = function (param) {
-        //console.log("change param = "+ param);
         $scope.sortparam=param;
     }
     
     $scope.renew=function(item) {}
     $scope.pdfeah=function(item) {
-        //console.log("pdf item = "+item.ID);
         var textlen = item.text.length;
         var docInfo = { 
          info: {
@@ -191,7 +173,7 @@ myApp.controller('gtpController', function($scope) {
         console.log("---fetch test end---");
     }
     init();
-    init2();
+    //init2();
 });
 myApp.controller('linkController', function($scope) {
     $scope.linkJson={
