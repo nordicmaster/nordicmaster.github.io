81e443d54846dbdf349a21c1a9a386f895765b92
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2020-07-22 11:32:59 +0300

    Update lastfmController.js

--- js/lastfmController.js
+++ js/lastfmController.js
@@ -3,37 +3,13 @@ myApp.controller('lastfmController', function($scope) {
 
     $scope.lastfmJson=[];
     
-    function ajax(url) {
-        //console.log("aja X");
-      return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                resolve(this.responseText);
-            }
-        };
-        xhr.onerror = reject;
-        xhr.open('GET', url);
-        xhr.send();
-      });
-    }
-    
-    $scope.getLastFm = function () {
-        ajax("https://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist=nordic+master&api_key=57ee3318536b23ee81d6b27e36997cde&format=json")
-          .then(function(result) {
-            var xres = JSON.parse(result);
-            $scope.lastfmJson = xres.toptracks.track;
-            console.log($scope.lastfmJson);
-          })
-          .catch(function() {
-            console.log("rerro2r-0lastfm");
-          });
-    }
-    
-    $scope.renew=function(item) {}
-            
-    init = function() {
-        $scope.getLastFm();
+    async function init() {
+        const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist=nordic+master&api_key=57ee3318536b23ee81d6b27e36997cde&format=json");
+        var xres = await response.json();
+        $scope.lastfmJson = Array.from(xres.toptracks.track);
+        $scope.$apply(function () {
+          ;
+        });     
     }
 
     init();
