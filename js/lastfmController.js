18cff08e2fff7a417ef25fc8a2a0e3561216f3c9
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2020-07-31 15:30:13 +0300

    Update lastfmController.js

--- js/lastfmController.js
+++ js/lastfmController.js
@@ -2,11 +2,17 @@ var myApp=angular.module('myApp');
 myApp.controller('lastfmController', function($scope) {
 
     $scope.lastfmJson=[];
+    $scope.listeners=0;
+    $scope.totalscrobbles=0;
     
     async function init() {
         const response = await fetch("https://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist=nordic+master&api_key=57ee3318536b23ee81d6b27e36997cde&format=json");
         var xres = await response.json();
         $scope.lastfmJson = Array.from(xres.toptracks.track);
+        const responseInfo = await fetch("https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=nordic+master&api_key=57ee3318536b23ee81d6b27e36997cde&format=json");
+        var xresInfo = await responseInfo.json();
+        $scope.listeners = xresInfo.artist.stats.listeners;
+        $scope.totalscrobbles = xresInfo.artist.stats.plays;
         $scope.$apply(function () {
           ;
         });     
