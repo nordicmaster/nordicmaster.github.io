8059affeb98f7da1e16e4d0cf40dab1de31b2f83
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-05-18 15:03:40 +0300

    Update reactGtpController.js

--- js/reactGtpController.js
+++ js/reactGtpController.js
@@ -1,6 +1,12 @@
+class Hello extends React.Component {
+        render() {
+            return <h1>Hello, React</h1>;
+        }
+    }
+
 ReactDOM.render(
         <div><h3>Hello React ea</h3>
-		<p>Время генерации данных: {new Date().toLocaleTimeString()}</p></div>,  // элемент, который мы хотим создать
-        document.getElementById("gtpController")    // где мы этот элемент хотим создать
+	<p>Время генерации данных: {new Date().toLocaleTimeString()}</p></div><Hello />,
+        document.getElementById("gtpController")
     );
 console.log("11");
