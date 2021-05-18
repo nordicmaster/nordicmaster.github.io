7ad0992e2f939819c4f0197a58979a41cc3d2df8
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-05-18 14:57:24 +0300

    Create reactGtpController.js

---
+++ js/reactGtpController.js
@@ -0,0 +1,6 @@
+ReactDOM.render(
+        <div><h3>Hello React ea</h3>
+		<p>Время генерации данных: {new Date().toLocaleTimeString()}</p></div>,  // элемент, который мы хотим создать
+        document.getElementById("gtpController")    // где мы этот элемент хотим создать
+    );
+console.log("11");
