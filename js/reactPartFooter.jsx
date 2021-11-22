96bb68d99d5bc9283f4f1cfc5d5c4301ebc3c34e
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-22 15:23:45 +0300

    Update reactPartFooter.jsx

--- js/reactPartFooter.jsx
+++ js/reactPartFooter.jsx
@@ -1,18 +1,17 @@
- import React from 'react';
-
- export const PartFooter = () => {
- return (
-  <div class="gtpmenu lineheight">
-    <nav>
-      <ul>
-          <li><p>___</p></li>
-          <li><p>contacts</p></li>
-          <li><p>NM productions </p></li>
-          <li><a href="stats.html">Statistics</a></li>
-          <li><a href="citations.html">Citations</a></li>
-          <li><p>___</p></li>
-      </ul>
-    </nav>
-  </div>
-)
+class PartFooter extends React.Component {
+  render() {
+    return <div class="gtpmenu lineheight">
+       <nav>
+         <ul>
+             <li><p>___</p></li>
+             <li><p>contacts</p></li>
+             <li><p>NM productions </p></li>
+             <li><a href="stats.html">Statistics</a></li>
+             <li><a href="citations.html">Citations</a></li>
+             <li><p>___</p></li>
+         </ul>
+       </nav>
+     </div>
+  }
 }
+ReactDOM.render(<PartFooter />, document.getElementById("part_footer"));
