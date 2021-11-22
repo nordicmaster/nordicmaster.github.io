733fcdf221ff95f634865b1637eacc1839ef5726
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-22 15:14:03 +0300

    Update reactPartMenu.jsx

--- js/reactPartMenu.jsx
+++ js/reactPartMenu.jsx
@@ -1,19 +1,21 @@
  import React from 'react';
 
- export const PartMenu = () => {
- return (
-  <div class="gtpmenu lineheight">
-    <nav>
-      <ul>
-        <li><a href="index.html">Main</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
-        <li><a href="covers.html">NM Covers</a></li>
-        <li><a href="newdesign.html">NM Design</a></li>
-      </ul>
-    </nav>
-  </div>
-)
+class PartMenu extends React.Component {
+	render(){
+		return (
+		  <div class="gtpmenu lineheight">
+		    <nav>
+		      <ul>
+			<li><a href="index.html">Main</a></li>
+			<li><a href="about.html">About</a></li>
+			<li><a href="gallery.html">Gallery</a></li>
+			<li><a href="covers.html">NM Covers</a></li>
+			<li><a href="newdesign.html">NM Design</a></li>
+		      </ul>
+		    </nav>
+		  </div>
+		)
+	}
 }
 
  ReactDOM.render(
