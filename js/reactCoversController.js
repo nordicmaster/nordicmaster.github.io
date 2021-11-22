76a680978e931c86157c3ec180e541ced16c428f
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-22 15:37:45 +0300

    Update reactCoversController.js

--- js/reactCoversController.js
+++ js/reactCoversController.js
@@ -2,23 +2,29 @@
 class Hello extends React.Component {
 	constructor(props) {
 	  super(props);
-	  this.state = { data: [] };
+	  this.state = { data: [], totalplayed: 0, totalrecorded: 0};
 	}
 	
 	componentDidMount() {
 		fetch('https://nordicmaster.github.io/covers.json')
       		.then(res => res.json())
-      		.then(json => this.setState({ data: json }));	
+      		.then(json => this.setState({ data: json, totalplayed: 22, totalrecorded: 4}));	
 	}
 
         render() {
-            return <ul>
-		    {this.state.data.map(el => (
-            		<li>
-              		<b>{el.author}</b>: {el.name} - 
-            		</li>
-          	    ))}
-	  </ul>;
+            return <div>
+			<ul>
+			    {this.state.data.map(el => (
+				<li>
+				<b>{el.author}</b>: {el.name} - 
+				</li>
+			    ))}
+			</ul>
+			<p>
+			<div class="inlineblock fourthwidth marginleft">Total covers played: {totalplayed} </div>
+			<div class="inlineblock fourthwidth marginleft">Total covers recorded: {totalrecorded} </div>
+			</p>
+		</div>;
         }
     }
 
