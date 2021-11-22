ea432346d21f26ca39e7a17b75827fa1f8cc7602
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-22 14:40:40 +0300

    Create reactPartFooter.jsx

---
+++ js/reactPartFooter.jsx
@@ -0,0 +1,18 @@
+ import React from 'react';
+
+ export const PartFooter = () => {
+ return (
+  <div class="gtpmenu lineheight">
+    <nav>
+      <ul>
+          <li><p>___</p></li>
+          <li><p>contacts</p></li>
+          <li><p>NM productions </p></li>
+          <li><a href="stats.html">Statistics</a></li>
+          <li><a href="citations.html">Citations</a></li>
+          <li><p>___</p></li>
+      </ul>
+    </nav>
+  </div>
+)
+}
