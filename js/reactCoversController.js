fd396dfd4c5e546a5e6cd1c05425caa27f7e2dfc
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-26 12:37:55 +0300

    Update reactCoversController.js

--- js/reactCoversController.js
+++ js/reactCoversController.js
@@ -20,7 +20,7 @@ class Hello extends React.Component {
 					<b>{el.author}</b>: {el.name}
 				</div>
 				<div class="inlineblock marginleft" style={{width:'18%'}}>
-                     			<span style={{fontSize:'80%'}}>Recorded: {el.recorded}</span>
+                     			<span style={{fontSize:'80%'}}>Recorded: {el.recorded == true && "true"}</span>
                  		</div>
 			    </div>	
 			))}
