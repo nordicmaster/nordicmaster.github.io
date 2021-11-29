a66871df9e562b193c9925608fdcb29efe03f50f
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-29 15:11:49 +0300

    Update reactCoversController.js

--- js/reactCoversController.js
+++ js/reactCoversController.js
@@ -8,7 +8,17 @@ class Hello extends React.Component {
 	componentDidMount() {
 		fetch('https://nordicmaster.github.io/covers.json')
       		.then(res => res.json())
+		.then(json => addtextforcovers(json))
       		.then(json => this.setState({ data: json }));	
+	}
+	
+	addtextforcovers(jsoncovers) {
+		for (let jcover in jsoncovers)
+		{
+		    fetch('https://nordicmaster.github.io/src/txt/'+ jcover.name+ '.txt')
+			.then(txt => jcover.text = JSON.stringify(txt).trim().replace(/\\n+/g,"\n"));
+		}
+		return jsoncovers;
 	}
 
         render() {
@@ -18,6 +28,9 @@ class Hello extends React.Component {
 			     <div>
 				<div class="inlineblock marginleft" style={{width:'60%'}}>
 					<b>{el.author}</b>: {el.name} {el.recorded == true && <img src='../src/rec.png' />}
+				</div>
+				<div class="inlineblock marginleft" style={{width:'40%'}}>
+					<i>{el.text}</i>
 				</div>
 				<hr/>
 			    </div>	
