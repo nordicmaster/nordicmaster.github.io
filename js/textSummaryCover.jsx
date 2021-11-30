b2f2cbb65bf56785736a04c70052020b56c4831e
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-30 10:11:37 +0300

    Update textSummaryCover.jsx

--- js/textSummaryCover.jsx
+++ js/textSummaryCover.jsx
@@ -3,12 +3,12 @@ class TextSummaryCover extends React.Component {
 	render(){
 		return <div class="inlineblock fourthwidth marginleft" style={{width:'35%'}}>
                      <span style="vertical-align:top;">
-                         <button class="button-like bgcol1" onClick='pdfeah(item)'>Download text</button>
+                         <button class="button-like bgcol1">Download text</button>
                      </span>
                      <span>
                         <details class="inlineblock">
                          <summary class="button-like align-center bgcol3">Show text</summary>                     
-                          <p class="smalltext">{{item.text}}</p>
+                          <p class="smalltext">{this.props.greeting}</p>
                         </details>
                      </span>                     
                  </div>;		
