71f1a819a924bcd8b4a4f0731c7d0a311b5dd826
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-11-30 15:29:55 +0300

    Update textSummaryCover.jsx

--- js/textSummaryCover.jsx
+++ js/textSummaryCover.jsx
@@ -7,7 +7,8 @@ class TextSummaryCover extends React.Component {
 	componentDidMount() {
 		fetch('https://nordicmaster.github.io/src/txt/' + this.props.name + '.txt')
       		//.then(res => res.json())		
-		.then(json => this.setState({ data: json }));	
+		.then(json => this.setState({ data: json }))
+		.catch(console.log);	
 	}
 	
 	/*.then(async function adtext(json1) {
