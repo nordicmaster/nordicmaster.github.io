eb0062e53afb04b1642b09f5fc188917d7a7babd
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-08 08:31:22 +0300

    Update textSummaryCover.jsx

--- js/textSummaryCover.jsx
+++ js/textSummaryCover.jsx
@@ -10,29 +10,7 @@ class TextSummaryCover extends React.Component {
 		.then(json => this.setState({ data: json }))
 		.catch(console.log);	
 	}
-	
-	/*.then(async function adtext(json1) {
-			var promises_arr = [];
-			for (let jcover of json1)
-			{
-			    promises_arr.push(fetch('https://nordicmaster.github.io/src/txt/'+ jcover.name+ '.txt'));
-			}
-			var kwargs = await Promise.all(promises_arr);
-			
-			console.log("2");
-			console.log(kwargs);
-			console.log(typeof kwargs);
-			for (var i = 0; i < kwargs.length; i++)
-			{
-			    console.log(kwargs[i]);
-			    await kwargs[i].json().then(data => json1[i].text = data).catch(console.log);
-			}
-			
-			console.log("3");
-			console.log(json1);
-			return json1;
-		})*/
-	
+		
 	render(){
 		return <div class="inlineblock marginleft" style={{width:'35%'}}>
                      <span style={{verticalAlign: 'top'}}>
