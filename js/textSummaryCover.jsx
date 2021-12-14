84088c26b1fed65b43aa4bba9ece3cce2610c530
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-14 13:03:39 +0300

    Update textSummaryCover.jsx

--- js/textSummaryCover.jsx
+++ js/textSummaryCover.jsx
@@ -1,52 +1,3 @@
-function pdfeah(name, text) {
-	var textlen = text.length;
-	var docInfo = { 
-		info: {
-			title:name+' Text',
-			author:'Master',
-			subject:'Theme',
-			keywords:'Ключевые слова'
-		},
-
-		pageSize:'A4',
-		pageOrientation:'portrait',//'landscape'
-		pageMargins:[50,50,30,60],
-
-		header:function(currentPage,pageCount) {
-			return {
-				text: currentPage.toString() + ' of ' + pageCount,
-				alignment:'right',
-				margin:[10,20,20,30]
-			}
-		},
-
-		footer:[
-			{
-				text:'NM production',
-				alignment:'center',//left  right
-			}
-		],
-
-		content: [
-			{
-				text:name,
-				fontSize:22,
-				alignment:'center',
-				margin:[10,10, 10,10]
-				//pageBreak:'after'
-			},
-			{
-				text:text.substring(1,textlen-1),
-				fontSize:16,
-				margin:[20,40, 20,40]
-				//pageBreak:'after'
-			}
-		]
-	};
-	pdfMake.createPdf(docInfo).download(name+'.pdf');
-}
-
-
 class TextSummaryCover extends React.Component {  
 	constructor(props) {
 	  super(props);
@@ -58,6 +9,54 @@ class TextSummaryCover extends React.Component {
 			.then(res => res.text())		
 			.then(json => this.setState({ data: json }))
 			.catch(console.log);	
+	}
+	
+	function pdfeah(name, text) {
+		var textlen = text.length;
+		var docInfo = { 
+			info: {
+				title:name+' Text',
+				author:'Master',
+				subject:'Theme',
+				keywords:'Ключевые слова'
+			},
+
+			pageSize:'A4',
+			pageOrientation:'portrait',//'landscape'
+			pageMargins:[50,50,30,60],
+
+			header:function(currentPage,pageCount) {
+				return {
+					text: currentPage.toString() + ' of ' + pageCount,
+					alignment:'right',
+					margin:[10,20,20,30]
+				}
+			},
+
+			footer:[
+				{
+					text:'NM production',
+					alignment:'center',//left  right
+				}
+			],
+
+			content: [
+				{
+					text:name,
+					fontSize:22,
+					alignment:'center',
+					margin:[10,10, 10,10]
+					//pageBreak:'after'
+				},
+				{
+					text:text.substring(1,textlen-1),
+					fontSize:16,
+					margin:[20,40, 20,40]
+					//pageBreak:'after'
+				}
+			]
+		};
+		pdfMake.createPdf(docInfo).download(name+'.pdf');
 	}
 		
 	render(){
