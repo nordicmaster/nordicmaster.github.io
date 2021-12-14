6019fe58c25664fc512ae438e1792538d4cd78e9
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-14 11:34:51 +0300

    Update textSummaryCover.jsx

--- js/textSummaryCover.jsx
+++ js/textSummaryCover.jsx
@@ -1,31 +1,3 @@
-class TextSummaryCover extends React.Component {  
-	constructor(props) {
-	  super(props);
-	  this.state = { data: "data" };
-	}
-	
-	componentDidMount() {
-		fetch('https://nordicmaster.github.io/src/txt/' + this.props.name + '.txt')
-			.then(res => res.text())		
-			.then(json => this.setState({ data: json }))
-			.catch(console.log);	
-	}
-		
-	render(){
-		return <div class="inlineblock marginleft" style={{width:'35%'}}>
-                     <span style={{verticalAlign: 'top'}}>
-                         <button class="button-like bgcol1" onclick='pdfeah(this.props.name, this.state.data)'>Download text</button>
-                     </span>
-                     <span>
-                        <details class="inlineblock">
-                         <summary class="button-like align-center bgcol3">Show text</summary>                     
-                          <p class="smalltext">{this.state.data}</p>
-                        </details>
-                     </span>                     
-                 </div>;		
-	}
-}
-
 pdfeah=function(name, text) {
 	var textlen = text.length;
 	var docInfo = { 
@@ -72,4 +44,33 @@ pdfeah=function(name, text) {
 		]
 	};
 	pdfMake.createPdf(docInfo).download(name+'.pdf');
+}
+
+
+class TextSummaryCover extends React.Component {  
+	constructor(props) {
+	  super(props);
+	  this.state = { data: "data" };
+	}
+	
+	componentDidMount() {
+		fetch('https://nordicmaster.github.io/src/txt/' + this.props.name + '.txt')
+			.then(res => res.text())		
+			.then(json => this.setState({ data: json }))
+			.catch(console.log);	
+	}
+		
+	render(){
+		return <div class="inlineblock marginleft" style={{width:'35%'}}>
+                     <span style={{verticalAlign: 'top'}}>
+                         <button class="button-like bgcol1" onclick='pdfeah(this.props.name, this.state.data)'>Download text</button>
+                     </span>
+                     <span>
+                        <details class="inlineblock">
+                         <summary class="button-like align-center bgcol3">Show text</summary>                     
+                          <p class="smalltext">{this.state.data}</p>
+                        </details>
+                     </span>                     
+                 </div>;		
+	}
 }
