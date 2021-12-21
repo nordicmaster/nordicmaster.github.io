d78016767e984d7e0c3c88dd45725f7e528f3b9c
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-21 09:27:44 +0300

    Create reactIndexController.js

---
+++ js/reactIndexController.js
@@ -0,0 +1,33 @@
+class Indexx extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = { data: [] };
+	}
+	
+	componentDidMount() {
+		fetch('https://nordicmaster.github.io/table_items.json')
+      		.then(res => res.json())		
+		.then(json => this.setState({ data: json }));	
+	}
+
+        render() {
+		this.state.data.sort((a, b) => a.author.localeCompare(b.author));
+            return <div>
+		        {this.state.data.map(el => (
+			     <div>
+				<div class="inlineblock marginleft" style={{width:'60%'}}>
+					<b>{el.author}</b>: {el.name} {el.recorded == true && <img src='../src/rec.png' />}
+				</div>
+				<TextSummaryCover name={el.name} />
+				<hr/>
+			    </div>	
+			))}
+			<p>
+				<div class="inlineblock fourthwidth marginleft">Total covers played: {this.state.data.length} </div>
+				<div class="inlineblock fourthwidth marginleft">Total covers recorded: {this.state.data.filter(x => x.recorded).length} </div>
+			</p>
+		</div>;
+        }
+    }
+
+ReactDOM.render(<Indexx />,document.getElementById("IndexController"));
