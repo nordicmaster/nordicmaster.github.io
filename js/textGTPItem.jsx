c2c23f7e5ee6b83deb9be3a7b0e630bf8bef659c
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-21 12:00:57 +0300

    Create textGTPItem.jsx

---
+++ js/textGTPItem.jsx
@@ -0,0 +1,78 @@
+class TextGTPItem extends React.Component {  
+	constructor(props) {
+	  super(props);
+	  this.state = { data: "data" };
+	  this.pdfeah = this.pdfeah.bind(this);//react magic
+	}
+	
+	componentDidMount() {
+		fetch('https://nordicmaster.github.io/src/txt/' + this.props.name + '.txt')
+			.then(res => res.text())		
+			.then(json => this.setState({ data: json }))
+			.catch(console.log);	
+	}
+	
+	pdfeah(name, text) {
+		console.log('name is:', name);
+		console.log('text is:', text);
+		var textlen = text.length;
+		var docInfo = { 
+			info: {
+				title:name+' Text',
+				author:'Master',
+				subject:'Theme',
+				keywords:'Ключевые слова'
+			},
+
+			pageSize:'A4',
+			pageOrientation:'portrait',//'landscape'
+			pageMargins:[50,50,30,60],
+
+			header:function(currentPage,pageCount) {
+				return {
+					text: currentPage.toString() + ' of ' + pageCount,
+					alignment:'right',
+					margin:[10,20,20,30]
+				}
+			},
+
+			footer:[
+				{
+					text:'NM production',
+					alignment:'center',//left  right
+				}
+			],
+
+			content: [
+				{
+					text:name,
+					fontSize:22,
+					alignment:'center',
+					margin:[10,10, 10,10]
+					//pageBreak:'after'
+				},
+				{
+					text:text.substring(0, textlen-1),
+					fontSize:16,
+					margin:[20,40, 20,40]
+					//pageBreak:'after'
+				}
+			]
+		};
+		pdfMake.createPdf(docInfo).download(name+'.pdf');
+	}
+		
+	render(){
+		return <div class="inlineblock marginleft" style={{width:'35%'}}>
+                     <span style={{verticalAlign: 'top'}}>
+                         <button class="button-like bgcol1" onClick={this.pdfeah.bind(this, this.props.name, this.state.data)}>Download text</button>
+                     </span>
+                     <span>
+                        <details class="inlineblock">
+                         <summary class="button-like align-center bgcol3">Show text</summary>                     
+                          <p class="smalltext">{this.state.data}</p>
+                        </details>
+                     </span>                     
+                 </div>;		
+	}
+}
