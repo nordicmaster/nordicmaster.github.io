0715fbfc0d8dd30315afc9d42a8273ed39a1e2d9
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-22 11:29:45 +0300

    Update reactPartFooter.jsx

--- js/reactPartFooter.jsx
+++ js/reactPartFooter.jsx
@@ -1,17 +1,24 @@
 class PartFooter extends React.Component {
   render() {
-    return <div class="gtpmenu lineheight">
-       <nav>
-         <ul>
-             <li><p>___</p></li>
-             <li><p>contacts</p></li>
-             <li><p>NM productions </p></li>
-             <li><a href="stats.html">Statistics</a></li>
-             <li><a href="citations.html">Citations</a></li>
-             <li><p>___</p></li>
-         </ul>
-       </nav>
-     </div>
+    return 
+    <div>
+      			<a href="https://soundcloud.com/user-912533495-638345693"><img src='../src/soundcloud.png' /></a>
+            <a href="https://nordicmaster.bandcamp.com/"><img src='../src/bandcamp.png' /></a>
+    			  <a href="https://www.youtube.com/channel/UCqNjPvkYI3BqNstzePzeUMA"><img src='../src/youtube.png' /></a>
+            <a href="https://vk.com/club12899135"><img src='../src/vk.png' /></a>
+            <div class="gtpmenu lineheight">
+              <nav>
+                <ul>
+                  <li><p>___</p></li>
+                  <li><p>contacts</p></li>
+                  <li><p>NM productions </p></li>
+                  <li><a href="stats.html">Statistics</a></li>
+                  <li><a href="citations.html">Citations</a></li>
+                  <li><p>___</p></li>
+                </ul>
+              </nav>
+            </div>
+    </div>
   }
 }
 ReactDOM.render(<PartFooter />, document.getElementById("part_footer"));
