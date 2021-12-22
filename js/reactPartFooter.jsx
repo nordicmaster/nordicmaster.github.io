fbaba9f6300dd5103aa559825137a95171472f8d
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-12-22 11:39:15 +0300

    Update reactPartFooter.jsx

--- js/reactPartFooter.jsx
+++ js/reactPartFooter.jsx
@@ -1,10 +1,9 @@
 class PartFooter extends React.Component {
   render() {
-    return 
-    <div>
-      			<a href="https://soundcloud.com/user-912533495-638345693"><img src='../src/soundcloud.png' /></a>
+    return <div>
+      		<a href="https://soundcloud.com/user-912533495-638345693"><img src='../src/soundcloud.png' /></a>
             <a href="https://nordicmaster.bandcamp.com/"><img src='../src/bandcamp.png' /></a>
-    			  <a href="https://www.youtube.com/channel/UCqNjPvkYI3BqNstzePzeUMA"><img src='../src/youtube.png' /></a>
+    	    <a href="https://www.youtube.com/channel/UCqNjPvkYI3BqNstzePzeUMA"><img src='../src/youtube.png' /></a>
             <a href="https://vk.com/club12899135"><img src='../src/vk.png' /></a>
             <div class="gtpmenu lineheight">
               <nav>
