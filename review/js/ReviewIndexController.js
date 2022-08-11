ea972c5e852e8fa844d41fb72293a4272a35f93d
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2022-08-11 09:31:26 +0300

    Create ReviewIndexController.js

---
+++ review/js/ReviewIndexController.js
@@ -0,0 +1,16 @@
+class ReviewIndexx extends React.Component {
+	constructor(props) {
+	  super(props);
+	  this.state = { data: [] };
+	}
+	
+	componentDidMount() {
+		// fetch artist names from files, also score
+	}
+
+        render() {
+		          //for each artist - num of reviews, avg score
+        }
+    }
+
+ReactDOM.render(<ReviewIndexx />,document.getElementById("ReviewController"));
